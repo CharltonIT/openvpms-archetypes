b5cff71150f318980b696a4e4bc1c81cf57c6b6c
benjamincharlton <benjicharlton@gmail.com>
2014-07-16 13:01:01 +1000

    Merge branch 'remote-1.8'
    Conflicts:
    	src/archetypes/org/openvpms/archetype/document/entityRelationship.documentTemplatePrinter.adl
    	src/java/org/openvpms/archetype/rules/doc/DocumentException.java
    	src/java/org/openvpms/archetype/rules/doc/DocumentTemplate.java
    	src/java/org/openvpms/archetype/rules/doc/DocumentTemplatePrinter.java

--- src/java/org/openvpms/archetype/rules/doc/DocumentException.java
+++ src/java/org/openvpms/archetype/rules/doc/DocumentException.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.archetype.rules.doc;
@@ -25,8 +23,8 @@ import org.openvpms.component.system.common.exception.OpenVPMSException;
 /**
  * Document exceptions.
  *
- * @author Benjamin Charlton
- * 
+ * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
+ * @version $LastChangedDate: 2006-05-02 05:16:31Z $
  */
 public class DocumentException extends OpenVPMSException {
 
